88a75183d2c46b9541d07bdc77c0d22b7a96fe9c
shayansalehi1381 <shayansalehisut@gmail.com>
2024-07-03 17:27:17 +0330

    Barrier reset position added

--- Assets/Scripts/Barrier1.cs
+++ Assets/Scripts/Barrier1.cs
@@ -6,11 +6,12 @@ public class Barrier1 : MonoBehaviour
 {
     public int ballCollided = 0;
 
-    public List<BarrierBase> barrierBases = new List<BarrierBase>();   
+    public List<BarrierBase> barrierBases = new List<BarrierBase>();
+  
 
     public void clearAllText()
     {
-       for (int i = 0; i < barrierBases.Count; i++)
+        for (int i = 0; i < barrierBases.Count; i++)
         {
             BarrierBase barrierBase = barrierBases[i];
             barrierBase.text.text = null;
--- Assets/Scripts/BarrierBase.cs
+++ Assets/Scripts/BarrierBase.cs
@@ -9,13 +9,38 @@ public class BarrierBase : MonoBehaviour
 
     public Barrier1 barrier;
 
+    public float Speed = 2.5f;
+    public float maxX = 5.6f;
+
+    [SerializeField]
+    private float resetPositionX = -4f;
+
     public void Start()
     {
         barrier.barrierBases.Add(this);
     }
 
+    void Update()
+    {
+        moving();
+        CheckPositionAndSwitchLocation();
+    }
+
     public void SetText(string newText)
     {
-            text.text = newText;   
+        text.text = newText;
+    }
+
+    void moving()
+    {
+        transform.Translate(Vector3.down * Speed * Time.deltaTime);
+    }
+
+    void CheckPositionAndSwitchLocation()
+    {
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(resetPositionX, transform.position.y, transform.position.z);
+        }
     }
 }
